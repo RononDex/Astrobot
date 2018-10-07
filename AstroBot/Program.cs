039c0a2082367ae684998c8017321720de7e5e6f
Tino Heuberger <tinoheuberger@hotmail.com>
2018-10-07 14:27:36 +0200

    First test version

---
+++ AstroBot/Program.cs
@@ -0,0 +1,28 @@
+﻿using AwesomeChatBot.ApiWrapper;
+using AwesomeChatBot.DiscordWrapper;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+
+namespace AstroBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var settings = JObject.Parse(File.ReadAllText("appsettings.json"));
+            var discordToken = File.ReadAllText(settings["DiscordTokenPath"].Value<string>());
+
+            // Setup DI
+            var serviceProvider = new ServiceCollection()
+             .AddLogging()
+             .AddScoped<NLog.Logger>()
+             .AddSingleton<ApiWrapper>(new DiscordWrapper(discordToken))
+             .AddSingleton<AwesomeChatBot.AwesomeChatBot>()
+             .BuildServiceProvider();
+
+
+        }
+    }
+}
