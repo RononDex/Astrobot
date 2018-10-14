32ff3ebb7e7c6bae72a4ec17a5697c562c06fcb6
Tino Heuberger <tinoheuberger@hotmail.com>
2018-10-14 15:20:05 +0200

    First test of the new framework

---
+++ AstroBot/DiscordSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AstroBot
+{
+    class DiscordSettings
+    {
+        public string DiscordTokenPath { get; set; }
+
+    }
+}
--- AstroBot/Program.cs
+++ AstroBot/Program.cs
@@ -1,7 +1,10 @@
 ﻿using AwesomeChatBot.ApiWrapper;
 using AwesomeChatBot.DiscordWrapper;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Linq;
+using NLog.Extensions.Logging;
 using System;
 using System.IO;
 
@@ -11,18 +14,38 @@ namespace AstroBot
     {
         static void Main(string[] args)
         {
-            var settings = JObject.Parse(File.ReadAllText("appsettings.json"));
-            var discordToken = File.ReadAllText(settings["DiscordTokenPath"].Value<string>());
+            NLog.LogManager.LoadConfiguration("nlog.config");
 
-            // Setup DI
-            var serviceProvider = new ServiceCollection()
-             .AddLogging()
-             .AddScoped<NLog.Logger>()
-             .AddSingleton<ApiWrapper>(new DiscordWrapper(discordToken))
-             .AddSingleton<AwesomeChatBot.AwesomeChatBot>()
-             .BuildServiceProvider();
+            var services = new ServiceCollection();
+            services.AddLogging();
+            services.AddOptions();
 
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
 
+            IConfigurationRoot configuration = builder.Build();
+            
+            // Setup the bot framework
+            var discordSettings = new DiscordSettings();
+            configuration.GetSection("DiscordSettings").Bind(discordSettings);
+
+            var discordWrapper = new DiscordWrapper(File.ReadAllText(discordSettings.DiscordTokenPath));
+
+            var chatbotSettings = new AwesomeChatBot.AwesomeChatBotSettings();
+            configuration.GetSection("ChatBotSettings").Bind(chatbotSettings);
+
+            services.AddSingleton<ApiWrapper>(discordWrapper);
+
+            // Setup the logging framework
+            var provider = services.BuildServiceProvider();
+
+            var serviceProvider = services.BuildServiceProvider();
+            var factory = provider.GetService<ILoggerFactory>();
+            factory.AddNLog();
+
+            var chatbotFramework = new AwesomeChatBot.AwesomeChatBot(discordWrapper, provider.GetService<ILoggerFactory>(), chatbotSettings);
         }
     }
 }
