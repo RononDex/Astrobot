6c10748d4cbaf27824f6db91ff04962e92ee14c0
RononDex <tinoheuberger@hotmail.com>
2019-03-07 13:05:34 +0100

    Finished Simbad TAP result parsing

--- AstroBot/Simbad/SimbadTAPQueryResult.cs
+++ AstroBot/Simbad/SimbadTAPQueryResult.cs
@@ -30,18 +30,21 @@ namespace AstroBot.Simbad
         {
             var astronomicalObjects = new List<AstronomicalObject>();
 
-            var rows = tapResultText.Replace("\n", "").Split("\r");
+            var rows = tapResultText.Replace("\r", "").Split("\n");
 
             // If less than 3 rows --> no rows in the result (since header has 2 rows)
             if (rows.Length < 3)
                 return;
 
-            var headerRow = rows.First();
-            var columnNames = headerRow.Split("|", StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim());
+            var headerRow = rows.First().ToUpper();
+            var columnNames = headerRow.Split("|", StringSplitOptions.None).Select(x => x.Trim());
 
             foreach (var row in rows.Skip(2))
             {
-                var curRowColumns = row.Split("|", StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim());
+                if (string.IsNullOrWhiteSpace(row))
+                    continue;
+
+                var curRowColumns = row.Split("|", StringSplitOptions.None).Select(x => x.Trim());
 
                 var properties = ParseProperties(columnNames.ToArray(), curRowColumns.ToArray());
                 astronomicalObjects.Add(new AstronomicalObject(properties));
@@ -50,6 +53,12 @@ namespace AstroBot.Simbad
             AstronomicalObjects = astronomicalObjects;
         }
 
+        /// <summary>
+        /// Parses the row into properties for the astronomical object
+        /// </summary>
+        /// <param name="columnNames"></param>
+        /// <param name="values"></param>
+        /// <returns></returns>
         private Dictionary<string, object> ParseProperties(string[] columnNames, string[] values)
         {
             var properties = new Dictionary<string, object>();
