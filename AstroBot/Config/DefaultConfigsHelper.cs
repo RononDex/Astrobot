2cf742454a9914c3a7d4d60450bd4bcffe610d26
RononDex <tinoheuberger@hotmail.com>
2019-06-10 09:27:20 +0200

    Implement greet new users event with config values

--- AstroBot/Config/DefaultConfigsHelper.cs
+++ AstroBot/Config/DefaultConfigsHelper.cs
@@ -9,7 +9,9 @@ namespace AstroBot.Config
     {
         private static IReadOnlyList<ConfigValue> DefaultServerConfig => new List<ConfigValue>
         {
-            new ConfigValue { Key = "GreetingChannel", Value = null }
+            new ConfigValue { Key = "GreetNewUsersChannel", Value = null },
+            new ConfigValue { Key = "GreetNewUsers", Value = "false" },
+            new ConfigValue { Key = "GreetNewUsersMessage", Value = "Welcome @UserMention" }
         };
 
         /// <summary>
@@ -23,9 +25,20 @@ namespace AstroBot.Config
             foreach (var defaultConfigValue in DefaultServerConfig)
             {
                 if (existingEntries.Any(x => x.Key == defaultConfigValue.Key))
+                {
                     continue;
+                }
 
                 Globals.BotFramework.ConfigStore.SetConfigValue(defaultConfigValue.Key, defaultConfigValue.Value, server);
+            }
+
+            // Delete existing entries that are not listed in "DefaultServerConfig"
+            var entriesToDelete = existingEntries.Where(x => !DefaultServerConfig.Any(y => x.Key == y.Key)).ToList();
+
+            for (int i = 0; i < entriesToDelete.Count; i++)
+            {
+                var entryToDelete = entriesToDelete[i];
+                Globals.BotFramework.ConfigStore.DeleteConfigEntry(entryToDelete.Key, server);
             }
         }
     }
--- AstroBot/Events/UserServerEvents.cs
+++ AstroBot/Events/UserServerEvents.cs
@@ -1,4 +1,3 @@
-using System.Linq;
 using AwesomeChatBot.ApiWrapper;
 
 namespace AstroBot.Events
@@ -7,8 +6,14 @@ namespace AstroBot.Events
     {
         public static void UserJoinedServer(User user, Server server)
         {
-            var channels = server.GetAllChannelsAsync().Result;
-            _ = channels.First().SendMessageAsync($"Welcome {user.Name}");
+            var greetingsChannel = Globals.BotFramework.ConfigStore.GetConfigValue<string>("GreetNewUsersChannel", server);
+            if (Globals.BotFramework.ConfigStore.GetConfigValue<bool>("GreetNewUsers", server)
+                && greetingsChannel != null)
+            {
+                var channel = server.ResolveChannelAsync(greetingsChannel).Result;
+                var message = Globals.BotFramework.ConfigStore.GetConfigValue<string>("GreetNewUsersMessage", server);
+                _ = channel?.SendMessageAsync($"{message.Replace("@UserMention", user.GetMention())}");
+            }
         }
     }
 }
